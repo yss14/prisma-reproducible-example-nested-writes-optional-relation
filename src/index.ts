805c8f2f266c52fcb9d960ed02ee19cc62e4a8b9
Yannick Stachelscheid <yannick@pricenow.ch>
2022-07-22 13:33:40 +0200

    add result inline comments

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,37 @@ const main = async () => {
 
   console.log(JSON.stringify({ newGroupWithoutCapacity }, null, 2));
 
+  /*
+    Creation order matches the input order
+
+    {
+      "newGroupWithoutCapacity": {
+        "id": 9,
+        "name": "Group without capacity",
+        "slots": [
+          {
+            "id": 25,
+            "from": "2022-07-22T00:00:00.000Z",
+            "groupId": 9,
+            "capacityId": null
+          },
+          {
+            "id": 26,
+            "from": "2022-07-23T00:00:00.000Z",
+            "groupId": 9,
+            "capacityId": null
+          },
+          {
+            "id": 27,
+            "from": "2022-07-24T00:00:00.000Z",
+            "groupId": 9,
+            "capacityId": null
+          }
+        ]
+      }
+    }
+  */
+
   const newGroupWithCapacity = await prisma.group.create({
     data: {
       name: "Group with capacity",
@@ -49,6 +80,37 @@ const main = async () => {
   });
 
   console.log(JSON.stringify({ newGroupWithCapacity }, null, 2));
+
+  /*
+    Creation order is reversed input order
+
+    {
+      "newGroupWithCapacity": {
+        "id": 10,
+        "name": "Group with capacity",
+        "slots": [
+          {
+            "id": 28,
+            "from": "2022-07-24T00:00:00.000Z",
+            "groupId": 10,
+            "capacityId": 13
+          },
+          {
+            "id": 29,
+            "from": "2022-07-23T00:00:00.000Z",
+            "groupId": 10,
+            "capacityId": 14
+          },
+          {
+            "id": 30,
+            "from": "2022-07-22T00:00:00.000Z",
+            "groupId": 10,
+            "capacityId": 15
+          }
+        ]
+      }
+    }
+  */
 };
 
 main().catch(console.error);
